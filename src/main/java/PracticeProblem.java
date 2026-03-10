44523cf16330d42c75381b44070ccb3205b30cd0
weiyaliu <343445839@tdsb.ca>
2026-03-10 15:29:07 +0000

    yayyy

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -2,6 +2,29 @@ public class PracticeProblem {
 
 	public static void main(String args[]) {
 
+		System.out.println(sum(5, 3));
+	}
+
+	public static int sum (int num1, int num2){
+
+		int result;
+		result = num1 + num2;
+		return result;
+	}
+	
+	public static int difference (int num1, int num2){
+		int result = num1 - num2;
+		return result;
+	}
+
+	public static double product (double num1, double num2){
+		double result = num1 * num2;
+		return result;
+	}
+
+	public static String removeFirst (String word){
+		String newWord = word.substring(1);
+		return newWord;
 	}
 
 	//Create your functions here
